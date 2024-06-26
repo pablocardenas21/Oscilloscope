5555ce9df41a76ff7eb14a73c8b386ee267088a6
KarlaEsterilla <karla.esterilla@ucuenca.edu.ec>
2024-06-26 18:05:09 -0500

    ADC_con_Interrupcion
    Se muestrea a 3K los canales 1 y 2, y el resto se lee en el loop

--- ADC_con_interrupcion/src/main.cpp
+++ ADC_con_interrupcion/src/main.cpp
@@ -1,92 +1,84 @@
 #include <Arduino.h>
-#include <stdio.h>
-#include <stdint.h>
 
-//*****************Definicion de pines***********/
 
-#define pot_Volt_div        32    //ADC1
-#define pot_seg_div         33    //ADC1   
-#define pot_offset_div      25    //ADC2  
-#define sig_Ch1             35    //ADC1       
-#define sig_Ch2             26    //ADC2
-#define LED                 2     //LED_BUILT
-
-//**********************************************/
-
-//***********Inicializacion de variables********/
+#define sig_Ch1             34    //ADC1_6 GPIO 34     
+#define sig_Ch2             35    //ADC1_7 GPIO 35
+#define pot_Volt_div        25    //ADC2_8 GPIO 25
+#define pot_seg_div         26    //ADC2_9 GPIO 26 
+#define pot_offset_div      27    //ADC2_7 GPIO 27 
+#define LED                 2     //LED interno azul
 
 int AN_pot_Volt_div_res = 0;
 int AN_pot_seg_div_res  = 0;
 int AN_pot_offset_div_res = 0;
-int AN_sig_Ch1_res = 0;
-int AN_sig_Ch2_res = 0;
+volatile int AN_sig_Ch1_res = 0;
+volatile int AN_sig_Ch2_res = 0;
 
-int contador = 2500;
-
-//*********************************************/
+void setup(void);
 
 hw_timer_t *Timer0_Cfg = NULL;
 
-
-//********Funcion de interrupcion*************/
-
 void IRAM_ATTR Timer0_ISR()
 {
-  //contador = contador - 1;
-  
+
   AN_sig_Ch1_res = analogRead(sig_Ch1);
   AN_sig_Ch2_res = analogRead(sig_Ch2);
-  Serial.print("--- 1: ");
-  Serial.print(AN_sig_Ch1_res/1000.0);
-  Serial.print(" | --- 2: ");
-  Serial.print(AN_sig_Ch2_res/1000.0);
-  Serial.println();
+
 }
 
-//******************************************/
-
-  /*  Tiempo_interr = TimerTicks * (prescaler)/(reloj Hz)
-  //     0.0001 = TimerTicks * 80/80 000 000    */
 
 
-void setup() {
 
-  Serial.begin(230400);
 
-  pinMode(LED, OUTPUT);
+void loop(){
+
+  
+
+    //AN_sig_Ch1_res = adc1_get_raw(sig_Ch1);
+    //AN_sig_Ch2_res = adc1_get_raw(sig_Ch2);
+    
+
+    
+    AN_pot_seg_div_res = analogRead(pot_seg_div);
+    AN_pot_offset_div_res = analogRead(pot_offset_div);
+    AN_pot_Volt_div_res = analogRead(pot_Volt_div);
+    
+    
+    Serial.print("--- 1: ");
+    Serial.print(AN_sig_Ch1_res/1000.0);
+    Serial.print(" | --- 2: ");
+    Serial.print(AN_sig_Ch2_res/1000.0);
+
+    Serial.print(" | --- 3: ");
+    Serial.print(AN_pot_seg_div_res/1000.0);
+    Serial.print(" | --- 4: ");
+    Serial.print(AN_pot_offset_div_res/1000.0);
+     Serial.print(" | --- 5: ");
+    Serial.print(AN_pot_Volt_div_res);
+    Serial.println();
+
+
+
+  
+
+}
+
+void setup(void){
+  Serial.begin(115200);
+  analogSetClockDiv(1);
+
 
   Timer0_Cfg = timerBegin(0, 80, true);
   timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
-  timerAlarmWrite(Timer0_Cfg, 200, true);
+  timerAlarmWrite(Timer0_Cfg, 333, true);
   timerAlarmEnable(Timer0_Cfg);
 
-
-}
-
-void loop() {
-
-
-  AN_pot_Volt_div_res = analogRead(pot_Volt_div);
-  AN_pot_seg_div_res = analogRead(pot_seg_div);
-  AN_pot_offset_div_res = analogRead(pot_offset_div);
-
+  adcAttachPin(sig_Ch1 );
+  adcAttachPin(sig_Ch2 );
+  adcAttachPin(pot_Volt_div);
+  adcAttachPin(pot_seg_div);
+  adcAttachPin(pot_offset_div);
+  
  
 
- /*
-  Serial.print(" | --- 3: ");
-  Serial.print(AN_pot_Volt_div_res/1000.0);
-  Serial.print(" | --- 4: ");
-  Serial.print(AN_pot_seg_div_res/1000.0);
-  Serial.print(" | --- 5: ");
-  Serial.print(AN_pot_offset_div_res/1000.0);*/
-
-  
-
-  /*if (contador == 0)
-  {
-    digitalWrite(LED, !digitalRead(LED));
-    contador = 2500;
-  }*/
-
 }
-
